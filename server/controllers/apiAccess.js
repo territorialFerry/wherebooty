ccfa534dae33024566d9a6758785e64705228776
vokoshyv <ronsuij@gmail.com>
2016-05-09 11:59:43 -0500

    (fix) handles players who don't play ranked games

--- server/controllers/apiAccess.js
+++ server/controllers/apiAccess.js
@@ -47,9 +47,66 @@ var forExport = {
 
     rp('https://'+oldRegion+'.api.pvp.net/api/lol/'+oldRegion+'/v1.3/stats/by-summoner/'+summonerID+'/ranked?season=SEASON2016&api_key='+secrets.apiKey)
     .catch(function(error){
-      console.log("ERROR: ", error);
-      res.render('index', {error: 'username not found'});
+
+      var rankedData = {
+        champions: []
+      }
+
+      var usefulRank = {};
+
+      for (var i = 0; i < rankedData.champions.length; i++){
+        usefulRank[rankedData.champions[i].id] = {
+          rankedWins: rankedData.champions[i].stats['totalSessionsWon'], 
+          rankedLosts: rankedData.champions[i].stats['totalSessionsLost']
+        }
+      }
+
+      // console.log(usefulRank);
+
+      var playedThisSeason = {};
+
+      championMasteryData.map(function(champ){
+        champ['rankedWins'] = usefulRank[champ.championId]? usefulRank[champ.championId].rankedWins : undefined;
+        champ['rankedLosts'] = usefulRank[champ.championId]? usefulRank[champ.championId].rankedLosts : undefined;
+        champ['name'] = champInfo[champ.championId]['name'];
+        champ['key'] = champInfo[champ.championId]['key'];
+        champ['title'] = champInfo[champ.championId]['title'];
+
+        if (champ['championLevel'] >= 4){
+          champ['championLevel'] = true;
+        } else {
+          champ['championLevel'] = false;
+        }
+
+        playedThisSeason[champ.championId] = true;
+        return champ;
+      })
+
+      // console.log(playedThisSeason);
+      // console.log(championMasteryData);
+
+      for (var ID in champInfo){
+        if (playedThisSeason[ID] === undefined){
+          championMasteryData.push({
+            championId: ID, 
+            name: champInfo[ID]['name'], 
+            key: champInfo[ID]['key'], 
+            title: champInfo[ID]['title'], 
+          })
+        }
+      }
+      
+      var forRender = {};
+      forRender.championMasteryData = championMasteryData;
+      forRender.summonerName = oldUsername;
+      // console.log(forRender);
+      
+      console.log('blah');
+      res.render('summoner', {forRender: forRender});
       return;
+      // console.log("ERROR: ", error);
+      // res.render('index', {error: 'username not found'});
+      // return;
     })
     .then(function(body){
       body = JSON.parse(body);
@@ -105,7 +162,7 @@ var forExport = {
       var forRender = {};
       forRender.championMasteryData = championMasteryData;
       forRender.summonerName = oldUsername;
-      console.log(forRender);
+      // console.log(forRender);
 
       res.render('summoner', {forRender: forRender});
 
